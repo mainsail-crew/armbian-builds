00f62f0140e891b7152e8dc69dfe37843cf0d355
Stephan Wendel <me@stephanwe.de>
2023-03-10 20:59:53 +0100

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -2,8 +2,4 @@
 
 printf "\n\n Try to apply userpatches/customize-images.sh\n\n"
 
-apt-get update --allow-releaseinfo-change
-
-apt-get install --yes --assume-yes sudo apt-utils wget net-tools unzip
-
-apt-get upgrade --yes --assume-yes
+printf "Only a test ....."
